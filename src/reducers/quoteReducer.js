e6906b72ae22ee725f96543bb5a57f41389571d0
Nishan Bajracharya <nishanbajracharya@lftechnology.com>
2017-09-24 16:50:57 +0545

    Map API response to store

--- src/reducers/quoteReducer.js
+++ src/reducers/quoteReducer.js
@@ -3,7 +3,10 @@ import * as quoteActions from '../actions/quoteActions';
 export const quoteReducer = (state = {}, { type = undefined, payload }) => {
   switch (type) {
   case quoteActions.STORE_QUOTE:
-    return { title: payload.quote.quote, author: payload.quote.author };
+    return {
+      title: payload.quote && payload.quote.quote,
+      author: payload.quote && payload.quote.author,
+    };
   default:
     return state;
   }
